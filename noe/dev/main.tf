87621d06d11909eca9a3169d0b369de463ecd5b9
Ravi Malvia <ravi.malvia@clouddrove.com>
2025-12-10 00:14:03 +0530

    added workflows

---
+++ noe/dev/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    CreatedBy   = "GitHub-Actions"
+  }
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "test-rg"
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "East US"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "resource_group_id" {
+  description = "ID of the created resource group"
+  value       = azurerm_resource_group.main.id
+}
+
+output "location" {
+  description = "Location of the resource group"
+  value       = azurerm_resource_group.main.location
+}
